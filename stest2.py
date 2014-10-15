a3289d6d0beb20370583adaff9b58012d5bd220f
John Beale <beale@bealecorner.com>
2014-10-15 11:15:53 -0700

    increase proc. resolution, add dilation & 2x erosion to improve (x,y) object location

--- stest2.py
+++ stest2.py
@@ -12,7 +12,7 @@
 # Recommend to increase priority with 'sudo chrt -r -p 99 <pid>' 
 # to reduce variability of process scheduling delays
 #
-# 14 October 2014  J.Beale
+# 15 October 2014  J.Beale
 
 # To install needed Python components do:
 # sudo apt-get install python-picamera python-numpy python-scipy 
@@ -23,6 +23,7 @@ import picamera, time
 from datetime import datetime  # for 'daytime' string
 import numpy as np  # for number-crunching on arrays
 from scipy import ndimage  # for array center-of-mass (location of new object)
+from PIL import Image # to export debug array as bitmap image
 
 global running  # have we done the initial array processing yet?
 global vPause   # true when we should stop grabbing frames
@@ -31,7 +32,7 @@ global mCount   # how many motion events detected in this segment
 global framesLead # lead time allowed before end-of-GOP to stop sampling YUV
 
 
-picDir = "/run/shm/vid/"   # where to store still frames
+picDir = "/ram/"   # where to store still frames
 videoDir = "/ram/"   # where to store video files
 tmpDir = "/run/shm/" # where to store YUV frame buffer
 frameRate = 8   # how many frames per second to record in video
@@ -40,10 +41,12 @@ sizeGOP = 60 # number of I+P frames in one GOP
 nGOPs = 4  # (nGOPs * sizeGOP) frames will be in one H264 video segment
 framesLead = 1 # how many frames before end-of-GOP we need to stop analyzing
 mCalcInterval = 2.0/frameRate # seconds in between motion calculations
+settleTime = 10.0 # how many seconds to do averaging before motion detect is valid
+debugMap = False # set 'True' to generate debug motion-bitmap .png files in picDir
 
 cXRes = 1920   # camera capture X resolution (video file res)
 cYRes = 1080    # camera capture Y resolution
-dFactor = 3.0  # how many sigma above st.dev for diff value to qualify as motion pixel
+dFactor = 2.5  # how many sigma above st.dev for diff value to qualify as motion pixel
 stg = 160       # groupsize for rolling statistics
 pixThresh = 8  # how many novel pixels counts as an event
 # --------------------------------------------------
@@ -55,8 +58,8 @@ running = False  # have we done the initial array processing yet?
 
 # --------------------------------------------------------------------------------------
 # xsize and ysize are used in the internal motion algorithm, not in the .h264 video output
-xsize = 32 # YUV matrix output horizontal size will be multiple of 32
-ysize = 16 # YUV matrix output vertical size will be multiple of 16
+xsize = 96 # YUV matrix output horizontal size will be multiple of 32
+ysize = 32 # YUV matrix output vertical size will be multiple of 16
 pixvalScaleFactor = 65535/255.0  # multiply single-byte values by this factor
 
 # --------------------------------------------------
@@ -80,6 +83,9 @@ def date_gen(camera):
 def initMaps():
     global newmap, difmap, avgdif, mtStart, lastTime, stsum, sqsum, stdev
     global avgNovel, xcent, ycent
+    global fnum # count of debug images output
+    global settled  # True when initial scene averaging has settled out
+
     newmap = np.zeros((ysize,xsize),dtype=np.float32) # new image
     difmap = np.zeros((ysize,xsize),dtype=np.float32) # difference between new & avg
     stsum  = np.zeros((ysize,xsize),dtype=np.int32) # rolling average sum of pix values
@@ -92,6 +98,8 @@ def initMaps():
     avgNovel = 0  # average of all "novel" pixels
     xcent = 0 # average x,y location of new object(s)
     ycent = 0 #
+    fnum = 0 # count of debug bitmaps output
+    settled = False # have not yet settled out averaging
 
 # saveFrame(): save a JPEG file
 def saveFrame(camera):
@@ -112,6 +120,7 @@ def getFrame(camera):
 # processImage(): do some computations on low-res version of current image
 def processImage(camera):
     global running  # have we done initial array processing yet?
+    global settled  # True when initial scene averaging has settled out
     global stsum # (matrix) rolling average sum of pixvals
     global sqsum # (matrix) rolling average sum of squared pixvals
     global stdev # (matrix) rolling average standard deviation of pixels
@@ -119,6 +128,7 @@ def processImage(camera):
     global countPixels # how many pixels show novelty value this frame
     global avgNovel # average value of all 'novel' pixels > 0    
     global xcent, ycent # average location of detected object
+    global fnum # count of debug images output
 
     newmap = pixvalScaleFactor * getFrame(camera)  # current pixmap  
 
@@ -135,6 +145,13 @@ def processImage(camera):
 
     stsum = (stsum * sti1) + newmap           # rolling sum of most recent 'stg' images (approximately)
     sqsum = (sqsum * sti1) + np.power(newmap, 2) # rolling sum-of-squares of 'stg' images (approx)
+
+    if not settled:
+      runTime = time.time() - mtStart # how many seconds we have been running
+      if (runTime > settleTime):
+        settled = True
+      return # stop right here, don't need to preceed before settling for valid motion detect
+
     devsq = (stg * sqsum) - np.power(stsum, 2)  # variance, had better not be negative
     np.clip(devsq, 0.1, 1E15, out=devsq)  # force all elements to have minimum value = 0.1
 	# adding 1.0 * pixvalScaleFactor is just saying every pixel has at least one count of std.dev
@@ -145,9 +162,21 @@ def processImage(camera):
     changedPixels = np.extract(condition, novel)  # make a list containing only changed pixels
     countPixels = changedPixels.size
     if (countPixels > 0):  # found something! (at least one pixel's worth of something)
-      avgNovel = np.average(changedPixels)
+      avgNovel = int( np.average(changedPixels)) # clipping to integer still leaves plenty of precision
       np.clip(novel, 0.0, 1.0, out=novel) # force negative values to 0, and clip positive values to 1
-      (ycent, xcent) = ndimage.measurements.center_of_mass(novel) # (x,y) center of motion. x is horizontal axis on image
+      moMap = ndimage.binary_dilation(novel)
+      novel = ndimage.binary_erosion(moMap, iterations=2)
+      countPixels = np.count_nonzero(novel) # recount how many 'novel' pixels there are after dilation + erosion
+
+    if (countPixels > 0):
+      (ycent, xcent) = ndimage.measurements.center_of_mass(novel.astype(int)) # (x,y) center of motion. x is horizontal axis on image
+      if (debugMap):  # generate bitmaps showing location of novel pixels?
+        novel = novel * 65535 # rescale 0-1 to fullscale for black/white display
+        img = Image.fromarray(novel.astype(int))
+        fnumstr = "%03d" % fnum
+        novMapName = picDir + "A" + fnumstr + ".png"
+        img.save(novMapName)  # save as image for visual analysis
+        fnum = fnum + 1
     else:
       avgNovel = 0
       (xcent, ycent) = (0, 0)  # nothing to see here, apparently
@@ -305,11 +334,11 @@ with picamera.PiCamera() as camera:
           if (countPixels >= pixThresh):
 	    eventRelTime = time.time() - segTime  # number of seconds since start of current H264 segment
 	tRemain = mCalcInterval - (time.time() - tLoop)
-#	if (tRemain < 0) or (lastCP >= 1):
-#          print("%d, %5.3f, %5.1f, %5.3f, %4.1f,%4.1f, %s" % \
-#		(countPixels, (1.0*segFrameNumber)/frameRate, avgNovel, tRemain, xcent, ycent, daytime))
+	if (tRemain < 0) or (lastCP >= 1):
+          print("%d, %5.3f, %d, %5.3f, %4.1f,%4.1f, %s" % \
+		(countPixels, (1.0*segFrameNumber)/frameRate, avgNovel, tRemain, xcent, ycent, daytime))
 	if (not log.closed) and ( (tRemain < 0) or (lastCP >= 1)):
-          log.write("%d, %5.3f, %5.1f, %5.3f, %4.1f,%4.1f, %s\n" % \
+          log.write("%d, %5.3f, %d, %5.3f, %4.1f,%4.1f, %s\n" % \
 		(countPixels, (1.0*segFrameNumber)/frameRate, avgNovel, tRemain, xcent, ycent, daytime))
         lastCP = countPixels # remember the previous countPixels value
 	if (tRemain > 0):
