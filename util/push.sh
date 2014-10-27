2a43cf4d77afc2348189237f011ad2be1bec9bc8
John Beale <beale@bealecorner.com>
2014-10-26 21:57:40 -0700

    update push.sh to go after > 1 .h264 file exists, instead of 2

--- util/push.sh
+++ util/push.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Continually push oldest .h264 and .txt files in 'fDir' directory to remote host
-# delete files locally after sending
+# push oldest .h264 and .txt files in 'fDir' directory to remote host
 
 # flim = number of files in directory, when exceeded, causes a file to be moved
-flim=2
+flim=1
 
 # fDir = local directory, source of files
 fDir="/ram/"
@@ -12,22 +11,18 @@ fDir="/ram/"
 # toDir = directory on remote host, destination of files
 toDir="/media/sdb1/"
 
-# user and name of remote host
-# must change this to match your setup, like "pi@192.168.10.50"
-host="pi@192.168.1.56"
-
-
 while [ 1 ]; do
   fnum=$(ls /ram/*.h264 | wc -l)
   if [ "$fnum" -gt "$flim" ]; then
+    sleep 4
     oldFileFull=$(ls -rt "$fDir"*.h264 | head -1)
     # oldFileFull = [full pathname of oldest file]
     oldFile="${oldFileFull##*/}"
     fBase="${oldFile%.*}"
     fLog="$fBase.txt"
-    echo "files = $fnum  So, should move $fDir$oldFile and $fDir$fLog to $toDir"
-    nice -n 20 scp -c blowfish "$fDir$oldFile" "$host":\""$toDir$oldFile"\"
-    nice -n 20 scp -c blowfish "$fDir$fLog" "$host":\""$toDir$fLog"\"
+    # echo "files = $fnum  So, should move $fDir$oldFile and $fDir$fLog to $toDir"
+    nice -n 20 scp -c blowfish "$fDir$oldFile" pi@192.168.1.56:\""$toDir$oldFile"\"
+    nice -n 20 scp -c blowfish "$fDir$fLog" pi@192.168.1.56:\""$toDir$fLog"\"
     rm "$fDir$oldFile"
     rm "$fDir$fLog" 
   fi  
